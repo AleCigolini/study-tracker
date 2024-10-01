cbfbe6995f8c3f933562067b2caaf955dd5f2380
AleCigolini <cigolini.ale@gmail.com>
2024-10-01 09:25:36 -0300

    Added component to manage the start and stop time

---
+++ src/components/FormularioTopo.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" />
+            </div>
+            <div class="column">
+                <div class="is-flex is-align-items-center is-justify-content-space-between">
+                    <section>
+                        <strong>{{ tempoDecorrido }}</strong>
+                        <button class="button" @click="iniciar">
+                            <span class="icon">
+                                <i class="fas fa-play"></i>
+                            </span>
+                            <span>play</span>
+                        </button>
+                        <button class="button" @click="finalizar">
+                            <span class="icon">
+                                <i class="fas fa-stop"></i>
+                            </span>
+                            <span>stop</span>
+                        </button>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "FormularioTopo",
+    data() {
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0
+        }
+    },
+    // COMPUTED É UTILIZADO PARA VARIÁVEIS DINÂMICAS - MUDAM DE VALOR DEVIDO ALGUMA CONDIÇÃO
+    computed: {
+        tempoDecorrido(): string {
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11, 19);
+        }
+    },
+    methods: {
+        iniciar() {
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos += 1;
+            }, 1000);
+        },
+
+        finalizar() {
+            clearInterval(this.cronometro);
+        }
+    }
+})
+</script>
